c60bb927cf9b2077540c5ea786bb62e2a0a099fe
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2024-01-12 13:23:45 +0100

    fix: disable all elevation on buttons

--- test/smoke_tests/interactions_smoke_test.dart
+++ test/smoke_tests/interactions_smoke_test.dart
@@ -14,6 +14,9 @@ void main() {
           GoldenTestScenario(
             name: 'scenario_button',
             child: ElevatedButton(
+              style: const ButtonStyle(
+                elevation: MaterialStatePropertyAll(0),
+              ),
               onPressed: () {},
               onLongPress: () {},
               child: const Text('button'),
